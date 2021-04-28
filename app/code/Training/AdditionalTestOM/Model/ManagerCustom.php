2a1e374c5a718b0d5d56124f62d8889c0269db3b
bohdan nazmieyev <bohdan.nazmieyev@qlicks.nl>
2021-04-28 12:23:57 +0300

    [1.3]

---
+++ app/code/Training/AdditionalTestOM/Model/ManagerCustom.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Training\AdditionalTestOM\Model;
+
+use Training\TestOM\Model\ManagerInterface;
+
+class ManagerCustom implements ManagerInterface
+{
+    public function create()
+    {
+    }
+
+    public function get()
+    {
+    }
+}
