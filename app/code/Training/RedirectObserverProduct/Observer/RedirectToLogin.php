815cc3793cbeaeac7eb26c7ebf257d1bbe0a0cf6
bohdan nazmieyev <bohdan.nazmieyev@qlicks.nl>
2021-05-12 18:43:29 +0300

    [1.10]

---
+++ app/code/Training/RedirectObserverProduct/Observer/RedirectToLogin.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Training\RedirectObserverProduct\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+
+class RedirectToLogin implements ObserverInterface
+{
+    /**
+     * @var \Magento\Framework\App\Response\RedirectInterface
+     */
+    private $redirect;
+    /**
+     * @var \Magento\Framework\App\ActionFlag
+     */
+    private $actionFlag;
+
+    /** @var \Magento\Customer\Model\Session  */
+    private $customerSession;
+
+    /**
+     * @param \Magento\Framework\App\Response\RedirectInterface $redirect
+     * @param \Magento\Framework\App\ActionFlag $actionFlag
+     */
+
+    public function __construct(
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Framework\App\Response\RedirectInterface $redirect,
+        \Magento\Framework\App\ActionFlag $actionFlag
+    )
+    {
+        $this->customerSession = $customerSession;
+        $this->redirect = $redirect;
+        $this->actionFlag = $actionFlag;
+    }
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $request = $observer->getEvent()->getData('request');
+        if ($request->getFullActionName() == 'catalog_product_view') {
+            if (!$this->customerSession->isLoggedIn()) {
+                $controller = $observer->getData('controller_action');
+                $this->actionFlag->set('', \Magento\Framework\App\ActionInterface::FLAG_NO_DISPATCH, true);
+                $this->redirect->redirect($controller->getResponse(), 'customer/account/login');
+            }
+        }
+    }
+}
