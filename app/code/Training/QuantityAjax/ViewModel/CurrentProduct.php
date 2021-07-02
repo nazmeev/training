7dc858163830e38b0536985ba7c5a3cbb0fe0f6a
bohdan nazmieyev <bohdan.nazmieyev@qlicks.nl>
2021-07-02 18:43:20 +0300

    [4.5]

--- app/code/Training/QuantityAjax/ViewModel/CurrentProduct.php
+++ app/code/Training/QuantityAjax/ViewModel/CurrentProduct.php
@@ -2,15 +2,22 @@
 
 namespace Training\QuantityAjax\ViewModel;
 
+use Magento\Catalog\Model\Product;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class CurrentProduct implements ArgumentInterface
 {
-
     public function __construct()
     {
-
     }
 
+    /**
+     * @param Product $product
+     * @return int
+     */
+    public function getProductId(Product $product): int
+    {
+        return $product->getId();
+    }
 
 }
